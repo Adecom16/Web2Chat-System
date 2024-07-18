398be7b4c2d15d9d18759cfa832d2d40be92a727
adecom16 <adefokunadeoluwaisrael@gmail.com>
2024-07-18 15:34:39 +0100

    updating and correct mistake

--- config/service.js
+++ config/service.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
-const admin = require('firebase-admin');
 
 const serviceAccount = {
   type: process.env.GCLOUD_TYPE,
   project_id: process.env.GCLOUD_PROJECT_ID,
   private_key_id: process.env.GCLOUD_PRIVATE_KEY_ID,
-  private_key: process.env.GCLOUD_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fix for multiline key
+  private_key: process.env.GCLOUD_PRIVATE_KEY,
   client_email: process.env.GCLOUD_CLIENT_EMAIL,
   client_id: process.env.GCLOUD_CLIENT_ID,
   auth_uri: process.env.GCLOUD_AUTH_URI,
@@ -13,20 +12,6 @@ const serviceAccount = {
   auth_provider_x509_cert_url: process.env.GCLOUD_AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url: process.env.GCLOUD_CLIENT_X509_CERT_URL,
   universe_domain: process.env.GCLOUD_UNIVERSE_DOMAIN,
-};
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-exports.sendNotification = (token, payload) => {
-  admin.messaging().sendToDevice(token, payload)
-    .then(response => {
-      console.log('Successfully sent message:', response);
-    })
-    .catch(error => {
-      console.log('Error sending message:', error);
-    });
 };
 
 console.log(`Project ID: ${serviceAccount.project_id}`);
